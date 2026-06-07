a1af682e4e876ff889c232e8d1016559cfa09e51
Hodo <hodkarim64@gmail.com>
2026-06-07 12:29:20 -0700

    Add: python-return-statement - submission-0

---
+++ Python For Beginners/python-return-statement/submission-0.py
@@ -0,0 +1,10 @@
+#make function product that accepts 2 params & returns product
+
+def product(value_1, value_2):
+    return value_1 * value_2
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))

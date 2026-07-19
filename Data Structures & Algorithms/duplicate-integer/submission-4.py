590afc70bc0680c458d1711bbb23c081418500ac
Hodo <hodkarim64@gmail.com>
2026-07-19 15:36:28 -0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        new_set = set(nums)
+
+        if len(new_set) == len(nums):
+            return False
+        else:
+            return True
+
+'''
+given: integer array nums
+goal: return true if any value appears more than once, otherwise false
+
+solution:
+array to a set.
+len array to len of set
+'''
+

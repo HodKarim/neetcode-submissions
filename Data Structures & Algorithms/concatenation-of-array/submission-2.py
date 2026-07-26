f1df7306b5a8345a1279ea01468c23e5e4022465
Hodo <hodkarim64@gmail.com>
2026-07-26 13:10:02 -0700

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        nums2 = nums + nums
+        return nums2
+
+'''
+given an integer array nums of length n
+create array ans of length 2n where ans
+
+so literally nums2 = nums + nums
+'''

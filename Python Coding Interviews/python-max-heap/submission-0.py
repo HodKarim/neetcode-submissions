8bbda976f972e302dab99a7459380b7592483876
Hodo <hodkarim64@gmail.com>
2026-07-29 12:14:52 -0700

    Add: python-max-heap - submission-0

---
+++ Python Coding Interviews/python-max-heap/submission-0.py
@@ -0,0 +1,30 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    '''
+    takes list of ints and returns ints in reverse sorted order
+
+    push elements into list and turn element back into positive at same time
+    '''
+    listt = []
+    for i in range(len(nums)):
+        listt.append(nums[i]*(-1))
+    heapq.heapify(listt)
+
+    result = []
+
+    while listt:
+        result.append(-heapq.heappop(listt))
+    return result
+
+
+
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))

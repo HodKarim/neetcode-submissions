a1cd93e41162a6fe200ae3a3154a6655a889f36a
Hodo <hodkarim64@gmail.com>
2026-07-28 19:17:20 -0700

    Add: python-dict-items - submission-1

---
+++ Python Coding Interviews/python-dict-items/submission-1.py
@@ -0,0 +1,21 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    listt = []
+    for key, value in zip(age_dict.keys(), age_dict.values()):
+        item = (key, value)
+        listt.append(item)
+    return listt
+'''
+take dictionary of names and ages and return list 
+of tupes where each tuple contains a name and age
+prolly use zip?
+'''
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))

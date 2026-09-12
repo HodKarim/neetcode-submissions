491e8b3a2d79f8db9a1a1d13a8e26d4f5371d0b8
Hodo <169486221+HodKarim@users.noreply.github.com>
2026-09-11 18:57:38 -0700

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+'''
+given array of ints and integer target, return indices i and j st 
+nums[i] + nums[j] == target
+
+nums = [3,4,5,6], target = 7
+
+so double 4 loop looking for the answer
+
+[3,4,5,6]
+i
+       j
+
+
+
+'''
